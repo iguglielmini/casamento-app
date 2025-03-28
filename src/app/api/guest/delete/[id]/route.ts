e798520eaac738074aede2fd2cb67645f23e1460
Isaac Henrique <isaac@isaacsolutions.tech>
2025-03-28 11:29:29 -0300

    fix: api delete params

--- src/app/api/guest/delete/[id]/route.ts
+++ src/app/api/guest/delete/[id]/route.ts
@@ -1,13 +1,12 @@
-import { NextApiRequest } from "next";
-import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
 export async function DELETE(
-  request: NextApiRequest
+  request: NextRequest
 ) {
-  const { id } = request.query as never;
+  const { id } = await request.json();
   const guestId = Number.parseInt(id);
 
   try {
