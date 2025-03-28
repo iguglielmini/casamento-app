16a1598ec05bc54ee84946623f9330d0de86c794
Isaac Henrique <isaac@isaacsolutions.tech>
2025-03-28 14:58:02 -0300

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,7 @@ const eslintConfig = [
     rules: {
       "@next/next/no-img-element": "off",
       "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/no-unused-vars": "off",
     }
   }),
 ];
