7960ec63320e569721e0b7576550f6afa3b521e6
Italo Giovanni Pedroza Guglielmini <italo.guglielmini@gmail.com>
2025-04-06 13:29:28 -0300

    feat: ajustes

--- src/app/components/guestFormModal/guestFormModal.tsx
+++ src/app/components/guestFormModal/guestFormModal.tsx
@@ -38,7 +38,7 @@ export default function GuestFormModal({
     <>
       <button
         onClick={() => setIsOpen(true)}
-        className="bg-green-600 text-white px-4 py-2 rounded flex items-center gap-2 hover:bg-green-700 transition"
+        className="flex items-center justify-center w-full md:w-auto bg-green-600 text-white px-4 py-2 rounded gap-2 hover:bg-green-700 transition"
       >
         <PlusCircle className="w-5 h-5" />
         Adicionar Convidado
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -57,7 +57,7 @@ export default function Dashboard() {
           <button
             type="button"
             onClick={() => exportGuestsToCSV(guests)}
-            className="flex items-center bg-blue-600 hover:bg-blue-800 text-white px-4 py-2 rounded"
+            className="flex items-center justify-center bg-blue-600 hover:bg-blue-800 text-white px-4 py-2 rounded w-full md:w-auto"
           >
             <Download className="w-5 h-5 mr-2" />
             Exportar em CSV
@@ -66,7 +66,7 @@ export default function Dashboard() {
           <button
             type="button"
             onClick={logout}
-            className="bg-gray-600 hover:bg-gray-700  text-white px-4 py-2 rounded"
+            className="flex items-center justify-center w-full md:w-auto bg-gray-600 hover:bg-gray-700  text-white px-4 py-2 rounded"
           >
             Sair
           </button>
