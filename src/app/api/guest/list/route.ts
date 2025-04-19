9c885459edfbd6d2b31924b82ad133d8b741a209
Italo Giovanni Pedroza Guglielmini <italo.guglielmini@gmail.com>
2025-04-19 01:09:24 -0300

    fix: remove project

--- src/app/api/guest/list/route.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-import { NextResponse } from "next/server";
-
-const prisma = new PrismaClient();
-
-export async function GET() {
-  const guests = await prisma.guest.findMany();
-  return NextResponse.json(guests, { status: 200 });
-}
